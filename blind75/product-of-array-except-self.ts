091b201a454e7ed55f442985c1a44d23e890f637
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-09 17:47:59 -0500

    adding another problem

---
+++ blind75/product-of-array-except-self.ts
@@ -0,0 +1,20 @@
+function productExceptSelf(nums: number[]): number[] {
+  // we should make a forward pass
+  // then a backwards pass
+  const result: Array<number> = new Array(nums.length).fill(1);
+
+  for (let i = 1; i < nums.length; i++) {
+    result[i] = result[i - 1] * nums[i - 1];
+  }
+
+  let right = 1;
+
+  for (let j = nums.length - 1; j >= 0; j--) {
+    result[j] *= right;
+    right *= nums[j];
+  }
+
+  return result;
+}
+
+export { productExceptSelf };
