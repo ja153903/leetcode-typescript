3c323b3c17a1bc1d05544d16904c7f951095126a
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-14 00:13:53 -0500

    did a problem

---
+++ blind75/number-of-one-bits.ts
@@ -0,0 +1,17 @@
+function hammingWeight(n: number): number {
+  let count = 0;
+
+  while (n > 0) {
+    // if the rightmost bit is a 1, then increase the count
+    if ((n & 1) === 1) {
+      count++;
+    }
+
+    // do logical right bitshift
+    n = n >>> 1;
+  }
+
+  return count;
+}
+
+export { hammingWeight };
