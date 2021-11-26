5bc575ae578d8c1fa49bd92fa6dd31c82b5e105f
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-26 10:28:13 -0500

    solved problem

--- blind75/nonoverlapping-intervals.ts
+++ blind75/nonoverlapping-intervals.ts
@@ -1,3 +1,22 @@
-function eraseOverlapIntervals(intervals: number[][]): number {}
+function eraseOverlapIntervals(intervals: number[][]): number {
+  if (intervals.length === 0) {
+    return 0
+  }
+
+  // This is code to find out the maximum number of non-overlapping intervals
+  intervals.sort((a: number[], b: number[]) => a[1] - b[1])
+  let end = intervals[0][1]
+  let count = 1
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] >= end) {
+      end = intervals[i][1]
+      count += 1
+    }
+  }
+
+  // find the complement
+  return intervals.length - count
+}
 
 export { eraseOverlapIntervals }
