95b0076be7547b926fe7dd90af762769d69ce5c7
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-26 09:14:48 -0500

    For some reason the spacing breaks

---
+++ blind75/insert-interval.ts
@@ -0,0 +1,34 @@
+function insert(
+  intervals: number[][],
+  newInterval: number[],
+): number[][] {
+  const result: number[][] = []
+
+  let i = 0
+
+  // push all intervals that are less than the new interval
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i])
+    i += 1
+  }
+
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval = [
+      Math.min(newInterval[0], intervals[i][0]),
+      Math.max(newInterval[1], intervals[i][1]),
+    ]
+
+    i += 1
+  }
+
+  result.push(newInterval)
+
+  while (i < intervals.length) {
+    result.push(intervals[i])
+    i += 1
+  }
+
+  return result
+}
+
+export { insert }
