cc4366e7272e9a084eaaa0b0d6b71ba7df224e7e
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-11-25 20:38:20 -0500

    adding solution

--- blind75/longest-consecutive-sequence.ts
+++ blind75/longest-consecutive-sequence.ts
@@ -1,5 +1,20 @@
 // Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 // You must write an algorithm that runs in O(n) time.
-function longestConsecutive(nums: number[]): number {}
+function longestConsecutive(nums: number[]): number {
+  const set = new Set(nums)
+  let best = 0
+
+  for (const num of nums) {
+    if (!set.has(num - 1)) {
+      let y = num + 1
+      while (set.has(y)) {
+        y++
+      }
+      best = Math.max(best, y - num)
+    }
+  }
+
+  return best
+}
 
 export { longestConsecutive }
